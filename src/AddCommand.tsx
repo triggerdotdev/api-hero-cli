1d8f977358018eac0608c978ab4022ddc7be4267
Matt Aitken <matt@mattaitken.com>
2022-08-23 14:43:55 +0100

    Added the Select component as a test

--- src/AddCommand.tsx
+++ src/AddCommand.tsx
@@ -1,5 +1,5 @@
 import { Arg, Command, GlobalOptions } from "@boost/cli";
-import { Confirm } from "@boost/cli/react";
+import { Confirm, Select } from "@boost/cli/react";
 import { Box, Text } from "ink";
 import React from "react";
 
@@ -26,6 +26,25 @@ function Component({ query }: { query: string }) {
 		<Box flexDirection="column">
 			<Text>More text here</Text>
 			<Text>Yet More text here {query}</Text>
+			<Select
+				label="What is your favorite fruit?"
+				onSubmit={() => {}}
+				options={[
+					{ label: "🍎 Apple", value: "apple" },
+					{ label: "🍌 Banana", value: "banana" },
+					{ label: "🥥 Coconut", value: "coconut" },
+					{ label: "🍇 Grapes", value: "grapes" },
+					{ label: "🥝 Kiwi", value: "kiwi" },
+					{ label: "🍋 Lemon", value: "lemon" },
+					{ label: "🍈 Melon", value: "melon" },
+					{ label: "🍊 Orange", value: "orange" },
+					{ label: "🍑 Peach", value: "peach" },
+					{ label: "🍐 Pear", value: "pear" },
+					{ label: "🍍 Pineapple", value: "pineapple" },
+					{ label: "🍓 Strawberry", value: "strawberry" },
+					{ label: "🍉 Watermelon", value: "watermelon" },
+				]}
+			/>
 			<Confirm label="Do you want to continue?" onSubmit={() => {}} />
 		</Box>
 	);
